e85051aa3a3a4fa487ca85327e9c9c46439b063b
Eragon35 <antipin35@yahoo.com>
2021-02-24 23:23:41 +0300

    Added ConsoleCommand

--- src/main/scala/prog/ConnectionUtils/ConsoleCommand.scala
+++ src/main/scala/prog/ConnectionUtils/ConsoleCommand.scala
@@ -1,5 +1,8 @@
 package prog.ConnectionUtils
 
-object ConsoleCommand extends {
+object ConsoleCommand extends Enumeration {
+  type ConsoleCommand = Value
+  val help,  info ,show ,add, update, remove_by_id, clear, execute_script, remove_head, remove_greater, history,
+  remove_all_by_number_of_rooms, count_by_number_of_rooms, print_field_descending_view = Value
 
 }
